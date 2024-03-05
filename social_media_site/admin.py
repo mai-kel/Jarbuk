4b05b22af9b79f85646527550c2a2539ae33fcf9
Michal <michal.golawski.mg@gmail.com>
2024-03-05 21:27:34 +0100

    Added models to admin page

--- social_media_site/admin.py
+++ social_media_site/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Comment, Profile, PostLike, CommentLike, Post
 
-# Register your models here.
+
+class CommentInline(admin.TabularInline):
+    model=Comment
+
+class CommentLiketInline(admin.TabularInline):
+    model=CommentLike
+
+class PostLikeInline(admin.TabularInline):
+    model=PostLike
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'creation_date', 'text']
+    inlines=[CommentInline, PostLikeInline]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'creation_date', 'text']
+    inlines = [CommentLiketInline]
+
+@admin.register(PostLike)
+class PostLike(admin.ModelAdmin):
+    list_display = ['creation_date', 'from_who', 'post']
+
+@admin.register(CommentLike)
+class PostLike(admin.ModelAdmin):
+    list_display = ['creation_date', 'from_who', 'comment']
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date_of_birth', 'profile_photo', 'cover_photo']
