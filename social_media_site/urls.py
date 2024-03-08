9f1ac709267ac5b0f6d489fa177f3254c67ca2d9
Michal <michal.golawski.mg@gmail.com>
2024-03-08 23:27:04 +0100

    Added url pattern for posts_feed view

--- social_media_site/urls.py
+++ social_media_site/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'social_media_site'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
+    path('', views.posts_feed, name='posts_feed'),
 ]
