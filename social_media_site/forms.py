bdeb31ec539dc654ba03702bd6556731d5051829
Michal <michal.golawski.mg@gmail.com>
2024-03-10 02:19:35 +0100

    Added clean method to UseForm.

--- social_media_site/forms.py
+++ social_media_site/forms.py
@@ -1,5 +1,16 @@
 from django import forms
 
 class UserForm(forms.Form):
-    first_name = forms.CharField(label="First name", max_length=30)
-    last_name = forms.CharField(label="Last name", max_length=30)
+    first_name = forms.CharField(label="First name", max_length=30, required=False)
+    last_name = forms.CharField(label="Last name", max_length=30, required=False)
+
+    def clean(self):
+        cd = self.cleaned_data
+
+        first_name = cd["first_name"]
+        last_name = cd["last_name"]
+
+        if (not (first_name or last_name)):
+            raise forms.ValidationError("At least one field must be filled")
+
+        return cd
