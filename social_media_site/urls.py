b1af870b1c6fff5fd3d4eb74feb3811ea8047297
Michal <michal.golawski.mg@gmail.com>
2024-03-19 00:44:51 +0100

    Added registration view to url patterns

--- social_media_site/urls.py
+++ social_media_site/urls.py
@@ -7,6 +7,7 @@ app_name = 'social_media_site'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
     path('users/', views.search_for_users, name='users_search'),
     path('user/<int:id>/', views.user_detail, name='user_detail'),
     path('post/like/<int:id>', views.post_like, name='post_like'),
