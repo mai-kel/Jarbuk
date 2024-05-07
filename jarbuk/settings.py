16a3e07ecc877484cb34003f508c5852faa73838
Michal <michal.golawski.mg@gmail.com>
2024-05-07 22:01:46 +0200

    Added easy-thumbnails to applications in settings

--- jarbuk/settings.py
+++ jarbuk/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social_media_site.apps.SocialMediaSiteConfig'
+    'social_media_site.apps.SocialMediaSiteConfig',
+    'easy_thumbnails',
 ]
 
 MIDDLEWARE = [
