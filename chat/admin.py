a2473e63d58215df4fc213c4d2cc9f279c453aa0
Michal <michal.golawski.mg@gmail.com>
2024-07-24 21:02:06 +0200

    Registered chat models on admin page

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import GroupChat, PrivateChat, GroupMessage, PrivateMessage
 
-# Register your models here.
+admin.site.register(PrivateChat)
+admin.site.register(GroupChat)
+admin.site.register(GroupMessage)
+admin.site.register(PrivateMessage)
