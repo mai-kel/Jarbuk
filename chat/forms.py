4223669acc408adfac2a791cbc665c60138c8f09
Michal <michal.golawski.mg@gmail.com>
2024-09-20 16:55:51 +0200

    Created GroupChatForm

---
+++ chat/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import GroupChat
+
+class MultipleUsersField(forms.ModelMultipleChoiceField):
+    def label_from_instance(self, obj):
+        return f'{obj.first_name} {obj.last_name}'
+
+
+class SingleUserField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return f'{obj.first_name} {obj.last_name}'
+
+
+class GroupChatForm(forms.ModelForm):
+    class Meta:
+        model = GroupChat
+        fields = ['name', 'participants', 'group_image']
+
+    name = forms.CharField(max_length=50)
+    participants = MultipleUsersField(queryset=None)
+    group_image = forms.ImageField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        self.logged_user = kwargs.pop('logged_user')
+        super().__init__(*args, **kwargs)
+        self.fields['participants'].queryset = User.objects.filter(
+            profile__in=self.logged_user.profile.friends.all())
+
